8a726f99af395b1db16b45bc957dcc09bbad9ca6
fpdevil <singamsetty.sampath@gmail.com>
2025-11-24 22:48:42 -0600

    updated version for terraform

--- provider.tf
+++ provider.tf
@@ -1,13 +1,23 @@
+# This block configures Terraform itself, including which providers to install
+# and which version of Terraform to use to provision out infrastructure.
+# It is recommended to maintain this in a dedicated terraform.tf file
 terraform {
-  required_version = ">=1.11"
+  # required terraform version
+  required_version = ">=1.14"
+
+  # select the provider version
+  # source contains address of the provider in the Terraform Registry.
+  # here "hashicorp/aws" is shortened form of "registry.terraform.io/hashicorp/aws"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">=5.83.0"
+      version = "6.22.1"
     }
   }
 }
 
+# set the provider
 provider "aws" {
+  # Configuration options here
   region = "us-east-2"
 }
